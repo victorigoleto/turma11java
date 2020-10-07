7a5b52a1d59f88d99ef40dffabd06763593736d3
Victor Rigoleto <62704734+victorigoleto@users.noreply.github.com>
2020-10-07 20:50:19 -0300

    Add files via upload
    1 - Faça um programa que receba três inteiros e diga qual deles é o maior.
    
    2 - Faça um programa que entre com três números e coloque em ordem crescente.
    
    3 - Faça um programa que receba a idade de uma pessoa e mostre na saída em qual categoria ela se encontra:
    10-14 infantil
    15-17 juvenil
    18-25 adulto
     
    4 - Faça um programa em que permita a entrada de um número qualquer e exiba se este número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for ímpar exiba o número elevado ao quadrado.

---
+++ java/estudo/src/aplicacoes/Lista2/Lista2Exercicio4.java
@@ -0,0 +1,33 @@
+package aplicacoes;
+
+import java.util.Scanner;
+
+public class Lista2Exercicio4 {
+
+	public static void main(String[] args) {
+		/*
+		 4 - Faça um programa em que permita a entrada de um número qualquer e 
+		 exiba se este número é par ou ímpar. Se for par exiba também a raiz 
+		 quadrada do mesmo; se for ímpar exiba o número elevado ao quadrado.
+		 */
+		
+		//variaveis
+		double n = 0;
+		
+		//input
+		Scanner tec = new Scanner(System.in);
+		System.out.print("Informe um numero: ");
+		n = tec.nextDouble();
+		
+		
+		//laço de decisão
+		if((n%2)==0) {
+			System.out.printf("O numero é PAR e a raiz quadrada dele é %.2f", Math.sqrt(n));
+		}
+		else {
+			System.out.printf("O numero é ÍMPAR e ao quadrado fica %.2f", Math.pow(n,2));
+		}
+
+	}
+
+}
