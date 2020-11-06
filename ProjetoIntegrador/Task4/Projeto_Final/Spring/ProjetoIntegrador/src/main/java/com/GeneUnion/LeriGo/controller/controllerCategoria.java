8433db8e75a8f2dc7f703ec0f1f24ecb83565d9e
Victor Rigoleto <62704734+victorigoleto@users.noreply.github.com>
2020-11-06 15:09:28 -0300

    Add files via upload

---
+++ ProjetoIntegrador/Task4/Projeto_Final/Spring/ProjetoIntegrador/src/main/java/com/GeneUnion/LeriGo/controller/controllerCategoria.java
@@ -0,0 +1,8 @@
+package com.GeneUnion.LeriGo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllerCategoria {
+
+}
---
+++ ProjetoIntegrador/Task4/Projeto_Final/Spring/ProjetoIntegrador/src/main/java/com/GeneUnion/LeriGo/repository/repositoryCategoria.java
@@ -0,0 +1,8 @@
+package com.GeneUnion.LeriGo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.GeneUnion.LeriGo.model.*;
+
+public interface repositoryCategoria extends JpaRepository<modelCategoria, Long>{
+
+}
